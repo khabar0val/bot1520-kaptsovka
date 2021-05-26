05266c1a5fe6e4f72d4ef48c2d59663e41757b5f
khabar0v <alexander.khabarov.123@gmail.com>
2021-05-27 00:08:44 +0300

    bot1520 testbuilt2 1.0.5v

--- src/botallclasses.py
+++ src/botallclasses.py
@@ -21,35 +21,33 @@ logger.add("bot1520log.log", format = "{time} {level} {message}", level = "WARNI
 
 @bot.message_handler(commands=['start'])
 def welcome2(message):
-    try:
+    # keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("👀 О школе")
+    item2 = types.KeyboardButton("🧑‍🏫 Расписание")
+    item3 = types.KeyboardButton("🐵 Поговорим?")
+    item4 = types.KeyboardButton("Связаться с...")
+    item5 = types.KeyboardButton("✅ Получить 5")
 
-        # keyboard
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1 = types.KeyboardButton("👀 О школе")
-        item2 = types.KeyboardButton("🧑‍🏫 Расписание")
-        item3 = types.KeyboardButton("🐵 Поговорим?")
-        item4 = types.KeyboardButton("Связаться с...")
-        item5 = types.KeyboardButton("✅ Получить 5")
+    markup.add(item1, item2, item3, item4, item5)
 
-        markup.add(item1, item2, item3, item4, item5)
+    bot.send_message(message.chat.id,
+                     "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы помогать вам с информацией о школе".format(
+                         message.from_user, bot.get_me()),
+                     parse_mode='html', reply_markup=marup)
 
-        bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы помогать вам с информацией о школе".format(message.from_user, bot.get_me()),
-            parse_mode='html', reply_markup=marup)
+    while True:
+        bot.send_message(message.chat.id,
+                         'https://www.instagram.com/kaptsovka/?hl=ru\n{0.first_name}, загляни в инстаграм Капцовки.\nВозможно там появилось что-то интересное!'.format(
+                             message.from_user, bot.get_me()),
+                         parse_mode='html')
 
-        while True:
+        bot.send_message(message.chat.id,
+                         'https://www.instagram.com/kaptsovschool/\n{0.first_name}, загляни в инстаграм Школы 1520 им. Капцовых.\nВозможно там появилось что-то интересное!'.format(
+                             message.from_user, bot.get_me()),
+                         parse_mode='html')
 
-            bot.send_message(message.chat.id, 'https://www.instagram.com/kaptsovka/?hl=ru\n{0.first_name}, загляни в инстаграм Капцовки.\nВозможно там появилось что-то интересное!'.format(message.from_user, bot.get_me()),
-                parse_mode='html')
-
-            bot.send_message(message.chat.id, 'https://www.instagram.com/kaptsovschool/\n{0.first_name}, загляни в инстаграм Школы 1520 им. Капцовых.\nВозможно там появилось что-то интересное!'.format(message.from_user, bot.get_me()),
-                parse_mode='html')
-
-            time.sleep(172800)
-
-    except:
-        logger.warning("WARNING with welcome2")
-        logger.error("ERROR with welcome2")
-        logger.critical("CRITICAL with welcome2")
+        time.sleep(172800)
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
